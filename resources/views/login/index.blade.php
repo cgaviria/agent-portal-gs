91dc4f5d4f7b75d12ad25612e0255b8076c85bd5
droyo\David Royo <davidroyobravo@gmail.com>
2018-04-02 16:32:52 -0500

    Add rememberme check on login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -31,6 +31,13 @@
                                     <label>Password</label>
                                 </div><span class="mda-input-group-addon"><em class="ion-ios-locked-outline icon-lg"></em></span>
                             </div>
+                            <div class="mda-form-group float-label mda-input-group">
+                                
+                                <input type="checkbox" name="keep_login" required="">
+                                <div class="mda-form-control-line"></div>
+                                <label>Remember me</label>
+                                
+                            </div>
 
                         </div>
                         <button class="btn btn-primary btn-flat" type="submit">Authenticate</button>
